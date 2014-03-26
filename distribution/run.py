7741ce5a7f29a3240d11515b2dc49a633c554146
mugur.rus <mugur.rus@sourcefabric.org>
2014-03-26 12:11:10 +0200

    Updated distribution configuration

--- distribution/run.py
+++ distribution/run.py
@@ -9,13 +9,9 @@ Created on Nov 24, 2011
 Provides the deployment of the distribution that contains this deploy.
 '''
 
-from types import ModuleType
-import argparse
 import os
 import sys
-import time
 import timeit
-import traceback
 
 # --------------------------------------------------------------------
 
@@ -23,19 +19,6 @@ if __name__ == '__main__':
     # First we need to set the working directory relative to the application deployer just in case the application is
     # started from somewhere else
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    
-    def findLibraries(folder):
-        '''Finds all the libraries (that have extension .egg or are folders) if the provided folder'''
-        if os.path.isdir(folder): return (os.path.join(folder, name) for name in os.listdir(folder))
-        return ()
-
-    # Loading the libraries
-    for path in findLibraries('libraries'):
-        if path not in sys.path: sys.path.append(path)
-
-    # Loading the components.
-    for path in findLibraries('components'):
-        if path not in sys.path: sys.path.append(path)
 
     try: import ally_start
     except ImportError:
